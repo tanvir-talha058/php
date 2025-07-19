76b37eb186568676a7f54320782f2a6035b97a30
Tanvir Ahmed <110832885+tanvir-talha058@users.noreply.github.com>
2025-07-19 10:01:54 +0600

    Add files via upload

---
+++ star problem with loop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    for($i=1; $i<=5; $i++)
+        {
+            for($j=1; $j<=$i; $j++)
+            echo "*";
+            echo "<br>";
+        }
+    echo "<br>";
+?>
+
+
+
+
+</body>
+</html>
