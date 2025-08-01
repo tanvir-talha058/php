70f03a251d4bbae9188331473a612a180f2e29aa
Tanvir <tanvirahmed123000@gmail.com>
2025-08-02 00:11:21 +0600

    constants

--- buildinFuncion.php
+++ buildinFuncion.php
@@ -4,4 +4,7 @@ echo strlen("hello tanvir") . "<br>";
 echo str_word_count("hello tanvir hi this is tanvir ahmed ") . "<br>";
 echo strrev("madam") . "<br>";
 
+//PHP String(Search For a Specific Text Within a String)
+echo strpos("hello tanvir", "hello");
+
 ?>
---
+++ php constants.php
@@ -0,0 +1,5 @@
+<?php
+ define("Greeting", "welome to php");
+
+ echo Greeting;
+ ?>
