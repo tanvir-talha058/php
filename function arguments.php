6a1bbeb3a5a47bdbf781603fd94a211d80eeb0e2
Tanvir <tanvirahmed123000@gmail.com>
2025-08-02 00:53:27 +0600

    basices

---
+++ function arguments.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    function familyName($fname){
+        echo "$fname refsnes. <br>";
+    }
+
+    familyName("Jani");
+    familyName("Hege");
+    familyName("Stale");
+    familyName("Kai Jim");
+    familyName("Borge");
+    familyName("Viggo");
+    familyName("Kim");
+    familyName("Tove");
+    familyName("Jørgen");
+
+    ?>
+</body>
+</html>
---
+++ operators.php
@@ -0,0 +1,12 @@
+<?php
+
+$x=10;
+$y=20;
+
+echo ($x + $y) ;
+echo ($x - $y) ;
+echo ($x * $y) ;
+echo ($x / $y) ;
+echo ($x % $y) ;
+
+?>
